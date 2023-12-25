43e14ce1886d834741328df3c058002d92af5d02
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-12-25 15:41:20 +0700

    Rest Api Response

--- src/main/java/hello/hellospring/HelloController.java
+++ src/main/java/hello/hellospring/HelloController.java
@@ -1,11 +1,13 @@
 package hello.hellospring;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+//@Controller
+@RestController
 public class HelloController {
     @GetMapping("hello")
     public String hello(Model model) {
@@ -17,5 +19,25 @@ public class HelloController {
     public String helloMvc(@RequestParam("name") String name, Model model) {
         model.addAttribute("name", name);
         return "hello-template";
+    }
+    
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name) {
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+    }
+    
+    static class Hello {
+        private String name;
+        
+        public String getName() {
+            return name;
+        }
+        
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
