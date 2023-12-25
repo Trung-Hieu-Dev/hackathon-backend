ce460e855c233b1ba81a628ae9eed83dc1133f8b
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-12-25 17:54:41 +0700

    Implement findByAll() and Write Test Case

--- src/test/java/hello/hellospring/Repository/MemoryMemberRepositoryTest.java
+++ src/test/java/hello/hellospring/Repository/MemoryMemberRepositoryTest.java
@@ -5,6 +5,8 @@ import hello.hellospring.repository.MemoryMemberRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class MemoryMemberRepositoryTest {
     
     MemoryMemberRepository memoryMemberRepository = new MemoryMemberRepository();
@@ -46,5 +48,27 @@ public class MemoryMemberRepositoryTest {
         // then
         // true member == find member
         Assertions.assertThat(result).isEqualTo(member1); // the result must be equal to initial
+    }
+    
+    @Test
+    public void findByAll() {
+        // given
+        // save 2 member
+        Member member1 = new Member();
+        member1.setName("Member A");
+        memoryMemberRepository.save(member1);
+        
+        Member member2 = new Member();
+        member2.setName("Member B");
+        memoryMemberRepository.save(member2);
+        
+        // when
+        // findAll
+        List<Member> result = memoryMemberRepository.findAll();
+        
+        // then
+        // count of members == 2
+        Assertions.assertThat(result.size()).isEqualTo(2);
+        
     }
 }
