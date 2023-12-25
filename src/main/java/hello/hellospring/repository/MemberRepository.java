9fcc94b8a4878c9dac8321fd25626ab04af72914
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-12-25 16:23:08 +0700

    Entity, Repository

---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,21 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    // registration
+    Member save(Member member);
+    
+    // Retrieval Id
+    Optional<Member> findById(Long id);
+    
+    // Retrieval name
+    Optional<Member> findByName(String name);
+    
+    // Retrieval All
+    List<Member> findAll();
+    
+}
