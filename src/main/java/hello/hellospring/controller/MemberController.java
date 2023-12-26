89651b9841cee4ef5679be83fd085cd8ae2b6fe3
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-12-26 15:49:56 +0700

    Spring MVC: Create and Get List of Members

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,9 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -22,4 +27,23 @@ public class MemberController {
     
     
     // member registration
+    @PostMapping("/members/new")
+    public String create(MemberForm memberForm) {
+        Member member = new Member();
+        member.setName(memberForm.getName());
+        
+        memberService.join(member);
+        
+        return "redirect:/";
+    }
+    
+    
+    // member list
+    @GetMapping("/members")
+    public String memberList(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        
+        return "members/memberList";
+    }
 }
