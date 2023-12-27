f56f1bc97edaafcdbef018157ae2993ce56ea2a4
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-12-27 11:30:39 +0700

    JdbcTemplate - config configuration

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,17 +1,25 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.MemberRepository;
-import hello.hellospring.repository.MemoryMemberRepository;
-import hello.hellospring.repository.MemoryPostRepository;
-import hello.hellospring.repository.PostRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
 import hello.hellospring.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 // create Beans obj
 @Configuration
 public class SpringConfig {
+    private final DataSource dataSource;
+    
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+    
+    
     // MemberService Bean obj
     @Bean
     public MemberService memberService() {
@@ -22,7 +30,8 @@ public class SpringConfig {
     // MemberRepository Bean obj
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        //return new MemoryMemberRepository(); // manually
+        return new JdbcTemplateRepository(dataSource); // using JdbcTemplate to work with DB
     }
     
     // PostService Bean obj
