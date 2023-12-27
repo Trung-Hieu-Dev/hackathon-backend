e0d88392637471bd3177c5239c07ff645e877b51
Trung-Hieu-Dev <dangtrunghieusg@gmail.com>
2023-12-27 11:22:18 +0700

    JdbcTemplate - saving to DB

--- src/main/java/hello/hellospring/repository/JdbcTemplateRepository.java
+++ src/main/java/hello/hellospring/repository/JdbcTemplateRepository.java
@@ -3,8 +3,11 @@ package hello.hellospring.repository;
 import hello.hellospring.domain.Member;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
 import javax.sql.DataSource;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,7 +19,22 @@ public class JdbcTemplateRepository implements MemberRepository {
     }
     
     @Override public Member save(Member member) {
-        return null;
+        // create SimpleJdbcInsert obj
+        SimpleJdbcInsert jdbcInsert = new SimpleJdbcInsert(jdbcTemplate);
+        jdbcInsert.withTableName("member")
+                  .usingGeneratedKeyColumns("id");
+        
+        // data
+        HashMap<String, Object> parameters = new HashMap<>();
+        parameters.put("name", member.getName());
+        
+        // save to DB and return memberId
+        Number memberId = jdbcInsert.executeAndReturnKey(new MapSqlParameterSource(parameters));
+        
+        // set member's id
+        member.setId((Long) memberId);
+        
+        return member;
     }
     
     @Override public Optional<Member> findById(Long id) {
